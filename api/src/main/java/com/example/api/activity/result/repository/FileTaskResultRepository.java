c6994edd356c9b3212e19e6f3fe8a856b35a8a3e
Ewa-M <92916895+Ewa-M@users.noreply.github.com>
2023-07-31 23:37:18 +0200

    [MAZE-60] - all points

--- api/src/main/java/com/example/api/activity/result/repository/FileTaskResultRepository.java
+++ api/src/main/java/com/example/api/activity/result/repository/FileTaskResultRepository.java
@@ -2,6 +2,7 @@ package com.example.api.activity.result.repository;
 
 import com.example.api.activity.result.model.FileTaskResult;
 import com.example.api.activity.task.model.FileTask;
+import com.example.api.course.model.Course;
 import com.example.api.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,5 +14,6 @@ public interface FileTaskResultRepository extends JpaRepository<FileTaskResult, 
     FileTaskResult findFileTaskResultById(Long id);
     FileTaskResult findFileTaskResultByFileTaskAndUser(FileTask fileTask, User user);
     List<FileTaskResult> findAllByUser(User user);
+    List<FileTaskResult> findAllByUserAndCourse(User user, Course course);
     List<FileTaskResult> findAllByFileTask(FileTask fileTask);
 }
--- api/src/main/java/com/example/api/activity/result/repository/SurveyResultRepository.java
+++ api/src/main/java/com/example/api/activity/result/repository/SurveyResultRepository.java
@@ -2,6 +2,7 @@ package com.example.api.activity.result.repository;
 
 import com.example.api.activity.result.model.SurveyResult;
 import com.example.api.activity.task.model.Survey;
+import com.example.api.course.model.Course;
 import com.example.api.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import java.util.List;
 @Repository
 public interface SurveyResultRepository extends JpaRepository<SurveyResult, Long> {
     List<SurveyResult> findAllByUser(User user);
-    SurveyResult findSurveyResultById(Long id);
+    List<SurveyResult> findAllByUserAndCourse(User user, Course course);
     SurveyResult findSurveyResultBySurveyAndUser(Survey survey, User user);
     List<SurveyResult> findAllBySurvey(Survey survey);
 }
