77f85793e23dfc6fc777a8cf27c200f1bbb1fd0c
Ewa-M <92916895+Ewa-M@users.noreply.github.com>
2023-08-03 15:24:07 +0200

    [MAZE-60] - ranks update - not tested

--- api/src/main/java/com/example/api/activity/result/controller/TaskResultController.java
+++ api/src/main/java/com/example/api/activity/result/controller/TaskResultController.java
@@ -12,7 +12,6 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
