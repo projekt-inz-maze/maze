b356ec130b15662d2c480bdd7ac7b5cc674699d5
Jakub Stępień <43409196+jkbstepien@users.noreply.github.com>
2023-09-08 17:00:16 +0200

    [MAZE-107] Implementacja nowego navbara (#14)

---
+++ frontend/src/components/general/Navbar/TopNavbar.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Container, Nav } from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
+import { Link, useNavigate } from 'react-router-dom'
+
+import styles from './TopNavbar.module.scss'
+import { logout } from '../../../actions/auth'
+
+
+type TopNavbarProps = {
+    sidebarTitles: { name: string, navigateTo: string }[]
+    userSubtitles: { name: string, navigateTo: string }[]
+}
+
+const TopNavbar = (props: TopNavbarProps) => {
+    const navigate = useNavigate()
+
+    return (
+        <Navbar expand='sm' className={`${styles.navbar} bg-body-tertiary`}>
+            <Container fluid style={{ width: '100%', margin: 0, padding: 0 }}>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav' className={styles.flow}>
+                    <Nav className={`${styles.upContainer}`}>
+                        <Nav.Link as={Link}
+                                  to={props.userSubtitles[0].navigateTo} className={styles.navBrand}>
+                            <FontAwesomeIcon icon={faFire} size='2x' style={{ color: '#ffb30d' }} />
+                            <span>Maze</span>
+                        </Nav.Link>
+                        <div className={styles.upContainer}>
+                            {props.sidebarTitles.map((link) => (
+                                <div key={link.navigateTo}>
+                                    <Nav.Link
+                                        as={Link}
+                                        to={link.navigateTo}
+                                        onClick={link.name === 'Wyloguj' ? logout(navigate) : undefined}
+                                    >
+                                        <span>{link.name}</span>
+                                    </Nav.Link>
+                                </div>
+                            ))}
+                        </div>
+                    </Nav>
+                    <Nav className={styles.downContainer}>
+                        {props.userSubtitles.map((link) => (
+                            <div key={`${link.navigateTo}userSubtitle`}>
+                                <Nav.Link
+                                    as={Link}
+                                    to={link.navigateTo}
+                                >
+                                    <span>{link.name}</span>
+                                </Nav.Link>
+                            </div>
+                        ))}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default TopNavbar
