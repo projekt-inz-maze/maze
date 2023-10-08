2eecf59aa27b90bbd0d8ac691a61097be351f59c
jkbstepien <devjakub.stepien@gmail.com>
2023-10-08 19:38:24 +0200

    [MAZE-111] Finish styling `ActivityDetails`

---
+++ frontend/src/common/components/ActivityDetails/ActivityDetails.tsx
@@ -0,0 +1,126 @@
+import React, { useState } from 'react'
+
+import { Button, Modal } from 'react-bootstrap'
+
+import styles from './ActivityDetails.module.scss'
+
+type ActivityDetailsProps = {
+  name: string
+  type: string
+  startDate: string
+  endDate: string
+  description: string
+  isHazard: boolean
+  numberOfAttempts: number
+  maxNumberOfAttempts: number
+  timeLimit: number
+  points: number
+  result: number
+}
+
+const ActivityDetails = (props: ActivityDetailsProps) => {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  return (
+    <>
+      <Button variant='primary' onClick={handleShow}>
+        Open Modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose} size='lg' className={styles.modalContainer}>
+        <Modal.Header closeButton className={styles.modalHeader}>
+          <Modal.Title className={styles.modalTitle}>Szczegóły aktywności</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className={styles.modalInfoSection}>
+            <div>
+              <p>
+                <span>Nazwa:</span> {props.name}
+              </p>
+              <p>
+                <span>Typ:</span> {props.type}
+              </p>
+            </div>
+            <div className={styles.dateInfo}>
+              <img
+                src='/map/calendar_icon_activity_details.png'
+                alt='calendar icon'
+                style={{ width: '40px', height: '40px' }}
+              />
+              <p>
+                <span>Data rozpoczęcia:</span> <br />
+                {props.startDate}
+              </p>
+              <p>
+                <span>Data zakończenia:</span> <br />
+                {props.endDate}
+              </p>
+            </div>
+          </div>
+          <div className={styles.modalTaskDescription}>
+            <p>Opis aktywności</p>
+            <p>{props.description}</p>
+          </div>
+          <div className={styles.modalTaskRequirements}>
+            <div className={styles.mainFlexContainer}>
+              <div className={styles.firstRow}>
+                <div className={styles.imgSection}>
+                  <img src='/icons/Thriller.png' alt='Hazard icon' />
+                  <div>
+                    <span>Hazard</span>
+                    <p>{props.isHazard ? 'Tak' : 'Nie'}</p>
+                  </div>
+                </div>
+                <div className={styles.imgSection}>
+                  <img src='/icons/Hashtag.png' alt='Hazard icon' />
+                  <div>
+                    <span>Liczba podejść</span>
+                    <p>
+                      {props.numberOfAttempts}/{props.maxNumberOfAttempts}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.secondRow}>
+                <div className={styles.imgSection}>
+                  <img src='/icons/Time.png' alt='Hazard icon' />
+                  <div>
+                    <span>Czas na rozwiązanie</span>
+                    <p>{props.timeLimit ? `${props.timeLimit} minut` : 'brak limitu czasowego'}</p>
+                  </div>
+                </div>
+                <div className={styles.imgSection}>
+                  <img src='/icons/Star.png' alt='Hazard icon' />
+                  <div>
+                    <span>Liczba punktów do zdobycia</span>
+                    <p>{props.points}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={props.result ? styles.resultFieldCompleted : styles.resultField}>
+              <span>Twój wynik:</span>
+              <p>{props.result ? `${props.result}/${props.points}` : 'nie ukończono'}</p>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer className={styles.modalFooter}>
+          <Button
+            variant='primary'
+            onClick={handleClose}
+            className={`${styles.startActivityButton} ${
+              props.maxNumberOfAttempts - props.numberOfAttempts ? '' : 'disabled'
+            }`}
+          >
+            Rozpocznij aktywność
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default ActivityDetails
