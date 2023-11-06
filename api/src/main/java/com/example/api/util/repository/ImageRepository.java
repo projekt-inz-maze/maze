d70925d2eb44b5cc80997a7aea35f13223aa926e
Ewa <emiklewska@gmail.com>
2023-11-06 16:16:18 +0100

    [MAZE-121] - hotfix

--- api/src/main/java/com/example/api/util/repository/ImageRepository.java
+++ api/src/main/java/com/example/api/util/repository/ImageRepository.java
@@ -1,6 +1,5 @@
 package com.example.api.util.repository;
 
-import com.example.api.course.model.Course;
 import com.example.api.util.model.Image;
 import com.example.api.util.model.ImageType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +8,6 @@ import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
     Image findImageById(Long id);
-    List<Image> findAllByCourse(Course course);
     List<Image> findAllByType(ImageType type);
 
 }
