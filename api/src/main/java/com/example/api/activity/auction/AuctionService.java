71ea74953eec08bdf2b723275c028f42d50750a3
Ewa <emiklewska@gmail.com>
2023-11-06 23:07:01 +0100

    [MAZE-128] - resolve draft

--- api/src/main/java/com/example/api/activity/auction/AuctionService.java
+++ api/src/main/java/com/example/api/activity/auction/AuctionService.java
@@ -4,11 +4,11 @@ import com.example.api.activity.auction.bid.Bid;
 import com.example.api.activity.auction.bid.BidDTO;
 import com.example.api.activity.auction.bid.BidRepository;
 import com.example.api.activity.task.Task;
+import com.example.api.chapter.requirement.model.StudentsRequirement;
 import com.example.api.error.exception.WrongUserTypeException;
 import com.example.api.map.ActivityMap;
 import com.example.api.user.model.User;
 import com.example.api.user.service.UserService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -48,5 +49,28 @@ public class AuctionService {
         Bid bid = new Bid(student, auction, dto.bidValue(), Instant.now());
         bidRepository.save(bid);
         auction.setHighestBid(bid);
+    }
+
+    public void resolveAuction(Auction auction) {
+
+        if (auction.getHighestBid().isPresent()) {
+            resolveHighestBid(auction);
+        } else {
+            resolveNoBids(auction);
+        }
+
+        auction.setResolved(true);
+    }
+
+    private void resolveNoBids(Auction auction) {
+        auction.setDescription("Nikt nie wziął udziału w aukcji.");
+    }
+
+    private void resolveHighestBid(Auction auction) {
+        User auctionWinner = auction.getHighestBid().map(Bid::getUser).get();
+        Task task = auction.getTask();
+
+        task.setIsBlocked(false);
+        task.getRequirements().add(new StudentsRequirement(auction.getTitle(), true, List.of(auctionWinner)));
     }
 }
