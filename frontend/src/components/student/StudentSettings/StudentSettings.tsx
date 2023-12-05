3ea9134c6688caee460cf4359ec4a67c98cdedc4
jkbstepien <devjakub.stepien@gmail.com>
2023-12-05 10:37:53 +0100

    [MAZE-169] feat: add initial view of StudentSettings

--- frontend/src/components/student/StudentSettings/StudentSettings.tsx
+++ frontend/src/components/student/StudentSettings/StudentSettings.tsx
@@ -1,7 +1,21 @@
-const StudentSettings = () => {
-  const placeholder = 'Student settings'
+import { Button, Container, Row } from 'react-bootstrap'
 
-  return <div>{placeholder}</div>
+import styles from './StudentSettings.module.scss'
+
+const StudentSettings = () => {
+  const placeholder = 'brak'
+
+  return (
+    <Container fluid className={styles.container}>
+      <Row>
+        <p className={styles.title}>Ustawienia</p>
+      </Row>
+      <div className={styles.items}>
+        <p>{`Typ osobowości: ${placeholder}`}</p>
+        <Button className={styles.testButton}>Wypełnij test!</Button>
+      </div>
+    </Container>
+  )
 }
 
 export default StudentSettings
