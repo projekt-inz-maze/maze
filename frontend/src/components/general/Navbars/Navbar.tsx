3fee1f861c3ab630ae21a8efffe4f5a14f435474
jkbstepien <devjakub.stepien@gmail.com>
2023-12-05 10:08:53 +0100

    [MAZE-169] style: remove comments in navbar

--- frontend/src/components/general/Navbars/Navbar.tsx
+++ frontend/src/components/general/Navbars/Navbar.tsx
@@ -1,28 +1,19 @@
 import PrimaryNavbar from './PrimaryNavbar/PrimaryNavbar'
-import { AddCourseRequest } from '../../../api/types'
-import { joinGroupRequest } from '../../../services/types/serviceTypes'
 
 type NavbarProps = {
   isStudent: boolean
   isProfessor: boolean
   navbarTitles: { name: string; navigateTo: string }[]
   userSubtitles: { name: string; navigateTo: string }[]
-  // dispatch: any
-  // userRole: number
-  // onAddCourse: (props: AddCourseRequest) => void
-  // onJoinCourse: (props: joinGroupRequest) => void
 }
 
-const Navbar = (props: NavbarProps) => {
-  const placeholder = true
-  return (
-    <PrimaryNavbar
-      isStudent={props.isStudent}
-      isProfessor={props.isProfessor}
-      navbarTitles={props.navbarTitles}
-      userSubtitles={props.userSubtitles}
-    />
-  )
-}
+const Navbar = (props: NavbarProps) => (
+  <PrimaryNavbar
+    isStudent={props.isStudent}
+    isProfessor={props.isProfessor}
+    navbarTitles={props.navbarTitles}
+    userSubtitles={props.userSubtitles}
+  />
+)
 
 export default Navbar
