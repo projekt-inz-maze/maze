00f77a23774ef20659ac0f51bd873ec50ad4c8f8
jkbstepien <devjakub.stepien@gmail.com>
2023-12-06 12:49:14 +0100

    [MAZE-169] feat: add quiz form demo using Formik

--- frontend/src/components/student/PersonalityQuiz/PersonalityQuizContent/PersonalityQuizContent.tsx
+++ frontend/src/components/student/PersonalityQuiz/PersonalityQuizContent/PersonalityQuizContent.tsx
@@ -1,25 +1,54 @@
-import { Container } from 'react-bootstrap'
+import { Field, Formik } from 'formik'
+import { Container, Form } from 'react-bootstrap'
 
+import styles from './PersonalityQuizContent.module.scss'
 import { QuizQuestion } from '../../../../api/types'
 
 type PersonalityQuizContentProps = {
   quiz: QuizQuestion[]
 }
+
 const PersonalityQuizContent = (props: PersonalityQuizContentProps) => {
-  const placeholder = 'Content'
+  const initialValues: Record<string, Record<string, boolean>> = {}
+
+  props.quiz.forEach((question, index) => {
+    initialValues[`question${index + 1}`] = {}
+
+    question.answers.forEach((answer, answerIndex) => {
+      initialValues[`question${index + 1}`][`answer${answerIndex + 1}`] = false
+    })
+  })
 
   return (
     <Container>
-      {props.quiz.map((question) => (
-        <div>
-          <p>{question.question}</p>
-          <div>
-            {question.answers.map((answer) => (
-              <p>{answer.content}</p>
-            ))}
-          </div>
-        </div>
-      ))}
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          console.log('User answers:', values)
+        }}
+      >
+        <Form>
+          {props.quiz.map((question, index) => (
+            <div key={index} className={styles.testQuestionContainer}>
+              <p className={styles.testQuestion}>{question.question}</p>
+              {question.answers.map((answer, answerIndex) => (
+                <div key={answerIndex}>
+                  <label className={styles.radioLabel}>
+                    <Field
+                      type='radio'
+                      name={`question${index + 1}`}
+                      value={`answer${answerIndex + 1}`}
+                      className={styles.radioInput}
+                    />
+                    {answer.content}
+                  </label>
+                </div>
+              ))}
+            </div>
+          ))}
+          <button type='submit'>Submit</button>
+        </Form>
+      </Formik>
     </Container>
   )
 }
