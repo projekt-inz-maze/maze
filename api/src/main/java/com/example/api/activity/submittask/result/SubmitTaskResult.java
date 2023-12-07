63c446fe2537de2f04365035829ba22df32af0d4
Ewa <emiklewska@gmail.com>
2023-12-07 23:53:23 +0100

    [MAZE-170] - added new endpoint with file

--- api/src/main/java/com/example/api/activity/submittask/result/SubmitTaskResult.java
+++ api/src/main/java/com/example/api/activity/submittask/result/SubmitTaskResult.java
@@ -3,12 +3,20 @@ package com.example.api.activity.submittask.result;
 import com.example.api.activity.result.model.ActivityResult;
 import com.example.api.activity.submittask.SubmitTask;
 import com.example.api.course.coursemember.CourseMember;
+import com.example.api.util.model.File;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import java.util.LinkedList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,9 +27,22 @@ public class SubmitTaskResult extends ActivityResult {
     private boolean evaluated = false;
 
     private String submittedTitle;
+
+    @Lob
     private String submittedContent;
 
+    @OneToMany
+    @JoinColumn
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<File> files = new LinkedList<>();
+
     public SubmitTaskResult(SubmitTaskResultDTO dto, CourseMember courseMember, SubmitTask task) {
+        super(courseMember, task);
+        this.submittedContent = dto.getContent();
+        this.submittedTitle = dto.getTitle();
+    }
+
+    public SubmitTaskResult(SubmitTaskResultWithFileDTO dto, CourseMember courseMember, SubmitTask task) {
         super(courseMember, task);
         this.submittedContent = dto.getContent();
         this.submittedTitle = dto.getTitle();
